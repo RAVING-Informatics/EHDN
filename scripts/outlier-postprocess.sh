a1b3a2ae3b3685d2c779dde05931f57e8f2757b4
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2025-09-25 15:17:11 +0800

    Update outlier-postprocess.sh

--- scripts/outlier-postprocess.sh
+++ scripts/outlier-postprocess.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 input_dir=$1
 
-python3 gene-counts.py $input_dir
+#python3 gene-counts.py $input_dir
 
-sed -i '1d' $input_dir/*.annotated.tsv
+sed -i '1d' $input_dir/*.outlier_locus.annotated.*
 
-ls $input_dir | grep '.annotated.tsv' | while read -r line ; do
+ls $input_dir | grep 'outlier_locus.annotated' | while read -r line ; do
 basename=${line%%.*}
-bedtools intersect -a $input_dir/$line -b $input_dir/*.annotated.tsv -c > $input_dir/${basename}_results.tsv 
-cat $input_dir/header.txt $input_dir/${basename}_results.tsv > $input_dir/${basename}_results_header.tsv 
+echo "finding intersect for $basename"
+bedtools intersect -a $input_dir/$line -b $input_dir/*.outlier_locus.annotated.* -c > $input_dir/${basename}_int.tsv 
+cat /data/ExpansionHunterDenovo/refs/header.tsv $input_dir/${basename}_int.tsv > $input_dir/${basename}_outlier_postprocessed.tsv 
+(head -n1 $input_dir/${basename}_outlier_postprocessed.tsv && tail -n +2 $input_dir/${basename}_outlier_postprocessed.tsv | sort -k7 -r ) > $input_dir/${basename}.outlier_postprocessed.sorted.tsv
+rm $input_dir/${basename}_int.tsv 
+rm $input_dir/${basename}_outlier_postprocessed.tsv
+echo "completed $basename"
 done
