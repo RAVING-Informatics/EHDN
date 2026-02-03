8fce21a78e00b07bc7944d2dac6ae32c3ff0f418
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2026-02-03 16:36:21 +0800

    Update submit-all.sh

--- scripts/submit-all.sh
+++ scripts/submit-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-input_dir=/scratch/pawsey0933/gmonahan/preprocessed/neuro/
+input_dir=/scratch/pawsey0933/gmonahan/preprocessed/rpl
 
-ls "$input_dir" | grep -E '\.bam$|\.cram$' | grep -v -E '\.bai$|\.crai$' | while read -r file; do
+ls "$input_dir" | grep -E '\.bam$|\.cram$' | grep -v -E '\.bai$|\.crai$|\.md5$' | while read -r file; do
     echo "Submitting job for $file"
     sbatch run-ehdn.sh "$file"
 done
